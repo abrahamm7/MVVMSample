866efd2ce68ae44a6538aa28eb6ec92f238bc678
Abraham Morillo <abrahammorillo777@gmail.com>
2020-02-18 21:12:30 -0400

    Binding Images

--- MVVMSample/MVVMSample/ViewModels/FoodPageViewModel.cs
+++ MVVMSample/MVVMSample/ViewModels/FoodPageViewModel.cs
@@ -15,10 +15,22 @@ namespace MVVMSample.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ICommand TapFrame { get; set; } 
-
+        public string cheesburguer { get; set; }
+        public string mercado { get; set; }
+        public string supermarket { get; set; }
+        public string vegetales { get; set; }
+        public string restaurante { get; set; }
+        public string food { get; set; }
+        
         public FoodPageViewModel()
         {
-            TapFrame = new Command(Tap);  
+            TapFrame = new Command(Tap);
+            cheesburguer = "cheeseburger.png";
+            mercado = "mercado.png";
+            supermarket = "Supermarketcar.png";
+            vegetales = "corn.png";
+            restaurante = "restaurant.png";
+            food = "food.png";
         }
 
         private void Tap(object sender)
